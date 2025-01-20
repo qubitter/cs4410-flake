50c33b2beb043b0c76748317233cbe34be9027b8
qubitter <11893617+qubitter@users.noreply.github.com>
2025-01-20 12:29:45 -0500

    specify nixpkgs unstable ver

--- flake.nix
+++ flake.nix
@@ -1,54 +1,53 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils"; 
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-
 
   outputs = {
     self,
     nixpkgs,
     ...
-  } @ inputs : 
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        name = "cs4410-flake";
-	src = ./.;
-      	pkgs = nixpkgs.legacyPackages.${system};
-        p3pkgs = pkgs.python312Packages;
-        ocaml-pkgs = with pkgs.ocamlPackages; [
-          findlib
-          extlib
-          ounit2
-          utop
-          odoc
-          ocaml-lsp
-          ocamlformat
-          ocamlbuild
-        ];
-        build-pkgs =  with pkgs; [
-          ocaml
-          dune_3
-          nasm
-          clang
-          valgrind
-        ]; 
-        grading-pkgs = with pkgs; [
-          unzip # to unzip student submissions :p
-          pyright # to develop my grading script
-          p3pkgs.orgparse # ditto
-        ];
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = build-pkgs ++ ocaml-pkgs;
-        };
-        devShells.grading = pkgs.mkShell {
-          buildInputs = build-pkgs ++ ocaml-pkgs ++ grading-pkgs;
-        };
-        packages = {
-          default = pkgs.writers.writePython3Bin "grading"
-            { libraries = [ p3pkgs.orgparse ]; }
-            (builtins.readFile ./grading.py);
-        };
-      });
-  
+  } @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      name = "cs4410-flake";
+      src = ./.;
+      pkgs = nixpkgs.legacyPackages.${system};
+      p3pkgs = pkgs.python312Packages;
+      ocaml-pkgs = with pkgs.ocamlPackages; [
+        findlib
+        extlib
+        ounit2
+        utop
+        odoc
+        ocaml-lsp
+        ocamlformat
+        ocamlbuild
+      ];
+      build-pkgs = with pkgs; [
+        ocaml
+        dune_3
+        nasm
+        clang
+        valgrind
+      ];
+      grading-pkgs = with pkgs; [
+        unzip # to unzip student submissions :p
+        pyright # to develop my grading script
+        p3pkgs.orgparse # ditto
+      ];
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = build-pkgs ++ ocaml-pkgs;
+      };
+      devShells.grading = pkgs.mkShell {
+        buildInputs = build-pkgs ++ ocaml-pkgs ++ grading-pkgs;
+      };
+      packages = {
+        default =
+          pkgs.writers.writePython3Bin "grading"
+          {libraries = [p3pkgs.orgparse];}
+          (builtins.readFile ./grading.py);
+      };
+    });
 }
